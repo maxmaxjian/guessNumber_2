a48a2b1c7807184aad86f2cce4d22840ebac3601
Jian Wei <jianwei@jianwei-deb8-64>
2017-01-04 12:40:39 -0500

    second version

--- main.cpp
+++ main.cpp
@@ -27,36 +27,44 @@ public:
         // std::copy(cands.begin(), cands.end(), std::ostream_iterator<int>(std::cout, " "));
         // std::cout << std::endl;
         // return *std::min_element(cands.begin(), cands.end());
-        return compute(1,n);
+
+        // return compute(1,n);
+
+        std::vector<int> result = compute(1,n);
+        return *std::min_element(result.begin(), result.end());
     }
 
 private:
-    int compute_impl(int i, int j) {
-	if (i == j)
-	    return 0;
-	else if (i == j-1)
-	    return std::max(i,j);
+    std::vector<int> compute_impl(int i, int j) {
+        std::vector<int> result;
+	if (i >= j)
+	    result.push_back(0);
 	else {
 	    std::vector<int> cands;
-	    cands.push_back(i+compute(i+1,j));
-	    for (int k = i+1; k < j; k++)
-		cands.push_back(k+std::max(compute(i,k-1), compute(k+1,j)));
-	    cands.push_back(j+compute(i,j-1));
-	    std::cout << "i = " << i << ", j = " << j << ":\t";
-	    std::copy(cands.begin(), cands.end(), std::ostream_iterator<int>(std::cout, " "));
-	    std::cout << std::endl;
-	    return *std::min_element(cands.begin(), cands.end());
+	    for (int k = i; k <= j; k++) {
+                auto v1 = compute(i,k-1), v2 = compute(k+1,j);
+		cands.push_back(k+std::max(*std::max_element(v1.begin(), v1.end()), *std::max_element(v2.begin(), v2.end())));
+            }
+	    // std::cout << "i = " << i << ", j = " << j << ":\t";
+	    // std::copy(cands.begin(), cands.end(), std::ostream_iterator<int>(std::cout, " "));
+	    // std::cout << std::endl;
+	    // return *std::min_element(cands.begin(), cands.end());
+            result = cands;
 	}
+        std::cout << "i = " << i << ", j = " << j << ":\t";
+        std::copy(result.begin(), result.end(), std::ostream_iterator<int>(std::cout, " "));
+        std::cout << std::endl;
+        return result;
     }
 
-    int compute(int i, int j) {
+    std::vector<int> compute(int i, int j) {
         if (dict.find(std::make_pair(i,j)) == dict.end())
             dict[std::make_pair(i,j)] = compute_impl(i,j);
         return dict[std::make_pair(i,j)];
     }
 
   private:
-    std::unordered_map<std::pair<int,int>,int> dict;
+    std::unordered_map<std::pair<int,int>,std::vector<int>> dict;
 };
 
 int main() {
